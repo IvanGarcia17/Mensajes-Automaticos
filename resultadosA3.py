700a6dda958b8bcc92a89d1f21cb2765b2482503
IvanGarcia17 <theivan_garcia17@hotmail.com>
2022-11-02 22:34:56 -0600

    Varios

---
+++ resultadosA3.py
@@ -0,0 +1,22 @@
+from turtle import width
+import pyautogui as pg, webbrowser as wb, time as tm, pandas as pd, os as os
+
+data = pd.read_csv("resultadosA3.csv")
+data_dict = data.to_dict('list')
+telefonos = data_dict['telefono']
+mensajes = data_dict['mensaje']
+combo = zip(telefonos, mensajes)
+first = True
+
+for telefono, mensaje in combo:
+    wb.open("https://web.whatsapp.com/send?phone="+telefono+"&text="+mensaje)
+    tm.sleep(11)
+    if first:
+        first = False
+    tm.sleep(32)
+    pg.hotkey('ctrl', 'v')
+    tm.sleep(8)
+    pg.press('enter')
+    tm.sleep(7)
+    pg.hotkey('ctrl', 'w')
+    print("enviado")
